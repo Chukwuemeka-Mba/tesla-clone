c1884fa655150bbbc55a6b2b179440ded70ea415
Chukwuemeka Mba <76162087+Chukwuemeka-Mba@users.noreply.github.com>
2022-06-05 15:50:35 -0400

    :ambulance: depreciated react to react 16.0

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./App";
 import "./plugins/fontawesome";
 const container = document.getElementById("root");
-const root = createRoot(container);
 
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  container
 );
